8748744d99ed464df8bdef459907d219e8be376b
cln-m4rie <s.kikkawa0613@gmail.com>
2020-03-28 02:42:23 +0900

    fix warn

--- terraform/gcp/asia-northeast1/modules.tf
+++ terraform/gcp/asia-northeast1/modules.tf
@@ -8,15 +8,15 @@ module "pubsub" {
 
 module "scheduler" {
   source                = "../../modules/gcp/scheduler"
-  oriental_lounge_topic = module.pubsub.oriental_lounge_topic
+  oriental_lounge_topic = "${module.pubsub.oriental_lounge_topic}"
 }
 
 module "functions" {
   source                     = "../../modules/gcp/functions"
-  storage_bucket_name        = module.storage.storage_bucket_name
-  storage_object_name        = module.storage.storage_object_name
-  oriental_lounge_topic_name = module.pubsub.oriental_lounge_topic.name
-  slack_webhook_url          = var.webhook.url
+  storage_bucket_name        = "${module.storage.storage_bucket_name}"
+  storage_object_name        = "${module.storage.storage_object_name}"
+  oriental_lounge_topic_name = "${module.pubsub.oriental_lounge_topic.name}"
+  slack_webhook_url          = "${var.webhook.url}"
 }
 
 
--- terraform/modules/gcp/scheduler/main.tf
+++ terraform/modules/gcp/scheduler/main.tf
@@ -5,7 +5,7 @@ resource "google_cloud_scheduler_job" "oriental_lounge_scraping_app_job" {
   time_zone   = "Asia/Tokyo"
 
   pubsub_target {
-    topic_name = var.oriental_lounge_topic.id
-    data       = base64encode("test")
+    topic_name = "${var.oriental_lounge_topic.id}"
+    data       = "${base64encode("test")}"
   }
 }
